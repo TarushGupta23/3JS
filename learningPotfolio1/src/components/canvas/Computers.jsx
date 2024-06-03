317322d9cc316c4e6b7dc5c6c3e751e1f7294674
TarushGupta23 <tarush23.gupta@gmail.com>
2024-06-03 18:05:59 +0530

    learningPotfolio1 - complete hero section

--- learningPotfolio1/src/components/canvas/Computers.jsx
+++ learningPotfolio1/src/components/canvas/Computers.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { Suspense, useState, useEffect } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
+    const computer = useGLTF('./desktop_pc/scene.gltf')
+    return (
+        <mesh>
+            <hemisphereLight intensity={3} groundColor="black" />
+            <pointLight intensity={1} />
+            <spotLight
+                position={[-20, 50, 10]} 
+                angle={0.12} 
+                penumbra={1} 
+                intensity={1} 
+                castShadow
+                shadow-mapSize={1024}
+            />
+
+            <primitive 
+                object={computer.scene} 
+                scale={0.75}
+                position={[ 0, -3.25, -1.510 ]}
+                rotation={[ -0.01, -.2, -.1 ]}
+            />
+        </mesh>
+    )
 }
 
-export default Computers
+const ComputerCanvas = () => {
+    return (
+        <Canvas 
+            frameloop='demand' 
+            shadows
+            camera={{ position: [20, 3, 5], fov: 25 }} // fov : field of view, position: [x, y, z]
+            gl={{ preserveDrawingBuffer: true }} // need for proper rendering
+            >
+            
+            <Suspense fallback={<CanvasLoader/>}> {/* provides a loader while our modle is loading... */}
+                <OrbitControls // set controls for our model
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                    enableZoom={false} />
+
+                <Computers />
+
+            </Suspense>
+            <Preload all />
+
+        </Canvas>
+    )
+}
+
+export default ComputerCanvas
